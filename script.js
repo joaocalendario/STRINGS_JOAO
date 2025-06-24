22791a5ae5913ff5a3aa4dd2581fcb4a14cff33b
joaocalendario <joao.calegario.piedade@escola.pr.gov.br>
2025-06-24 15:08:32 +0000

    commit

--- script.js
+++ script.js
@@ -14,6 +14,18 @@ function mostraPalavrasChave() {
 function processaTexto(texto) {
   let palavras = texto.split(/\P{L}+/u);
 
+  const frequencias = contaFrequencias(palavras);
+
+  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+  function ordenaPalavra(p1, p2) {
+    return frequencias[p2] - frequencias[p1];
+  }
+
+  return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
   let frequencias = {};
 
   for (let i of palavras) {
@@ -28,5 +40,5 @@ function processaTexto(texto) {
 
   console.log(frequencias);
 
-  return palavras;
+  return frequencias;
 }
